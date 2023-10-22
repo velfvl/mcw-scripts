6af85bd4b7aab3c979d56b54e9fd8debaceb59de
vel <135318721+velfvl@users.noreply.github.com>
2023-10-22 16:49:08 +0800

    Update enemy autofarm.lua

--- enemy autofarm.lua
+++ enemy autofarm.lua
@@ -6,20 +6,38 @@ local function args(a)
 		[1] = "hit",
 		[2] = {
 			[1] = a,
-			[2] = a.Health/2
+			[2] = a.MaxHealth/4
 		}
 	}
 	return gs
 end
 
-while task.wait() do
-    coroutine.wrap(function()
-        for _,v in pairs(workspace:GetChildren()) do
-        	local humanoid = v:FindFirstChild("Humanoid")
-            if not game:GetService("Players"):GetPlayerFromCharacter(v) and humanoid and humanoid.Health ~= 0 then
-                local swad = character:FindFirstChild("Sword") or player.Backpack:FindFirstChild("Sword")
-                swad.RemoteFunction:InvokeServer(unpack(args(humanoid)))
-            end
-        end
-    end)()
+local function isaenemey(a)
+	for _,v in pairs(a:GetDescendants()) do
+		if v:IsA("Script") and v.Name == EnemyMain then
+			return true
+		end
+	end
 end
+
+local function kill(a)
+	local humanoid = a:FindFirstChild("Humanoid")
+	if isaenemy(a) and humanoid and humanoid.Health ~= 0 then
+		local swad = character:FindFirstChild("Sword") or player.Backpack:FindFirstChild("Sword")
+		task.wait(1)
+		swad.RemoteFunction:InvokeServer(unpack(args(humanoid)))
+	end
+end
+
+for _,v in pairs(workspace:GetChildren()) do
+	kill(v)
+end
+
+workspace.DescendantAdded:Connect(function(p)
+	local humanoid = p:FindFirstChild("Humanoid")
+	if not game:GetService("Players"):GetPlayerFromCharacter(v) and humanoid and humanoid.Health ~= 0 then
+		local swad = character:FindFirstChild("Sword") or player.Backpack:FindFirstChild("Sword")
+        task.wait(1)
+        swad.RemoteFunction:InvokeServer(unpack(args(humanoid)))
+	end
+end)
