842d77a9bfd7bdff4cd68e345cf191a72854d49e
vel <135318721+velfvl@users.noreply.github.com>
2023-10-22 18:05:58 +0800

    Update enemy autofarm.lua

--- enemy autofarm.lua
+++ enemy autofarm.lua
@@ -6,15 +6,15 @@ local function args(a)
 		[1] = "hit",
 		[2] = {
 			[1] = a,
-			[2] = a.MaxHealth/4
+			[2] = math.huge
 		}
 	}
 	return gs
 end
 
-local function isaenemey(a)
+local function isaenemy(a)
 	for _,v in pairs(a:GetDescendants()) do
-		if v:IsA("Script") and v.Name == EnemyMain then
+		if v:IsA("Script") and v.Name == "EnemyMain" then
 			return true
 		end
 	end
@@ -24,20 +24,15 @@ local function kill(a)
 	local humanoid = a:FindFirstChild("Humanoid")
 	if isaenemy(a) and humanoid and humanoid.Health ~= 0 then
 		local swad = character:FindFirstChild("Sword") or player.Backpack:FindFirstChild("Sword")
-		task.wait(1)
+		task.wait(1.5)
 		swad.RemoteFunction:InvokeServer(unpack(args(humanoid)))
 	end
 end
 
-for _,v in pairs(workspace:GetChildren()) do
-	kill(v)
-end
-
-workspace.DescendantAdded:Connect(function(p)
-	local humanoid = p:FindFirstChild("Humanoid")
-	if not game:GetService("Players"):GetPlayerFromCharacter(v) and humanoid and humanoid.Health ~= 0 then
-		local swad = character:FindFirstChild("Sword") or player.Backpack:FindFirstChild("Sword")
-        task.wait(1)
-        swad.RemoteFunction:InvokeServer(unpack(args(humanoid)))
+while task.wait() do
+	for _,v in pairs(workspace:GetChildren()) do
+		coroutine.wrap(function()
+			kill(v)
+		end)()
 	end
-end)
+end
